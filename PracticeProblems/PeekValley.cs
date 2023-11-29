1f178794fe8e0f669e2edc5921d858abd3f10fd7
Julio <Julio@DESKTOP-A02BVBH>
2023-11-29 18:07:03 -0500

    Add project files.

---
+++ PracticeProblems/PeekValley.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class PeekValley
+    {
+        public int[] setTerrain(int[] pk)
+        {
+            int[] tmp = new int[pk.Length];
+            Array.Sort(pk);
+            int j = 0;
+
+            for (int i = 0; i < pk.Length/2; i++)
+            {
+                tmp[j++] = pk[i];
+                tmp[j++] = pk[^(i+1)];
+            }
+
+            if (pk.Length % 2 == 1)
+                tmp[^1] = pk[pk.Length / 2];
+
+            return tmp;
+        }
+    }
+}
---
+++ PracticeProblems/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeProblems;
+
+PeekValley p = new PeekValley();
+int[] ar = { 19, 8, 12, 5, 1, 3, 11};
+
+Console.WriteLine("The Array");
+
+Array.ForEach(p.setTerrain(ar), t => Console.WriteLine(t));
