061e00315725ad8a04f903cff299fdf46137091f
juladams <134738588+juladams@users.noreply.github.com>
2023-12-01 16:39:05 -0500

    Update PeekValley.cs
    Added comments

--- PracticeProblems/PeekValley.cs
+++ PracticeProblems/PeekValley.cs
@@ -11,13 +11,18 @@ namespace PracticeProblems
         public int[] setTerrain(int[] pk)
         {
             int[] tmp = new int[pk.Length];
+            //Sort the array in ascending order
             Array.Sort(pk);
+            //temp array index
             int j = 0;
-
+            
+            //iterate through the array alternating least to greatest int
             for (int i = 0; i < pk.Length/2; i++)
             {
+                //inserting the least remaining int to next available pos
                 tmp[j++] = pk[i];
-                tmp[j++] = pk[^(i+1)];
+                //inserting the greatest remaining int to next available pos
+                tmp[j++] = pk[^(i+1)]; //from the end of the array
             }
 
             if (pk.Length % 2 == 1)
