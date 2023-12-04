e08e32fae202d844529142e17912f0a576000ee0
juladams <juladams@microsoft.com>
2023-12-04 09:24:02 -0500

    Anagram added

---
+++ PracticeProblems/Anagram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class Anagram
+    {
+        public bool isAnagram(string s1,  string s2) 
+        {
+            s1 = s1.ToLower();
+            if (s1.ToLower() == s2.ToLower()) return true;
+            else if(s1.Length != s2.Length) return false;
+            else
+            {
+                Dictionary<char, int> map = new Dictionary<char, int>();
+                foreach (char c in s1.ToLower())
+                {
+                    if (map.ContainsKey(c))
+                    {
+                        map[c] = map[c]++;
+                    }
+                    else
+                        map.Add(c, 1);
+                }
+
+                foreach (char c in s2.ToLower())
+                {
+                    if(!map.ContainsKey(c))
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- PracticeProblems/Program.cs
+++ PracticeProblems/Program.cs
@@ -1,9 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PracticeProblems;
 
-PeekValley p = new PeekValley();
+/*PeekValley p = new PeekValley();
 int[] ar = { 19, 8, 12, 5, 1, 3, 11};
 
 Console.WriteLine("The Array");
 
-Array.ForEach(p.setTerrain(ar), t => Console.WriteLine(t));
+Array.ForEach(p.setTerrain(ar), t => Console.WriteLine(t));*/
+
+Anagram a = new Anagram();
+
+string s1 = "Race";
+string s2 = "Care";
+string s3 = "Car";
+string s4 = "Cater";
+string s5 = "Recat";
+
+Console.WriteLine(a.isAnagram(s1, s2)); //true
+Console.WriteLine(a.isAnagram(s1, s3)); //false
+Console.WriteLine(a.isAnagram(s3, s4)); //false
+Console.WriteLine(a.isAnagram(s4, s5)); //true
