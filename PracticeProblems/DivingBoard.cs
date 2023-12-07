0ba10694b87b2b8d456457ebfb1f926297a87c6d
Juliene Adams <juladams@microsoft.com>
2023-12-07 09:50:30 -0500

    Added new method toboards calculate multiple

--- PracticeProblems/DivingBoard.cs
+++ PracticeProblems/DivingBoard.cs
@@ -16,10 +16,44 @@ namespace PracticeProblems
             //Calculate the possible lengths
             for (int i = 0; i <= k; i++)
             {
-                Console.WriteLine(s * i + l * j--);
+                Console.WriteLine("There are a total of {0} short boards of size {1} and {2} long boards of " +
+                    "size {3} for a total size of {4}"s*i, s, l*j, l, s * i + l * j--);
             }
             //((k+n-1)!)/((k!(n-1)!) = number of possiblities
             return k > 0 ? k + 1 : 0;
+        }
+
+        //assuming the boards are different lengths
+        public int boardLength2(int[] boardLengths, int numPlanks)
+        {
+            //number of possiblities if k is greater than 0
+            int p, count = (factorial(numPlanks + boardLengths.Length - 1)) / ((factorial(numPlanks)factorial(boardLengths.Length - 1));
+            //number of board sizes
+            int nL = boardLengths.Length;
+            //set to store the board combinations
+            HashSet<int> boards = new HashSet<int>();
+
+            while(count > 0)
+            {
+                //Calculate the possible lengths
+                for (int i = 0; i <= numPlanks; i++)
+                {
+                    Console.WriteLine(s * i + l * j--);
+                }
+            }
+
+            
+            //(factorial(k+n-1))/((factorial(k)factorial(n-1)) = number of possiblities
+            return numPlanks > 0 ? p : 0;
+        }
+
+        public int factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * factorial(n - 1);
+
         }
     }
 }
