3b0fab411dd42e555ab8a2daff68dcae7bb58f29
juladams <juladams@microsoft.com>
2024-01-08 09:52:40 -0500

    RugLayers

---
+++ PracticeProblems/OddishEvenish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class OddishEvenish
+    {
+        public string oddishOrEvenish(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum % 2 == 0 ? "Evenish" : "Oddish";
+        }   
+    }
+}
---
+++ PracticeProblems/RugLayers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblems
+{
+    internal class RugLayers
+    {
+        public int rugLayers(string[] n)
+        {
+            int count = 0;
+            int i = 0;
+            HashSet<string> layers = new HashSet<string>();
+
+            while (!layers.Contains(n[i]) && i > n.Length)
+            {
+                layers.Add(n[i++]);
+                count++;
+            }
+            return count;
+        }
+    }
+}
